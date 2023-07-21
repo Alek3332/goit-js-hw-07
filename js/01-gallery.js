0a7b0aac0ad8390463b0832fec0f71d7aaf964c9
Alek3332 <118377866+Alek3332@users.noreply.github.com>
2023-07-21 13:20:32 +0300

    213

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,9 +6,10 @@ galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
 galleryContainer.addEventListener('click', onImgClick);
 
 function createGalleryItemsMarkup(items) {
+
     return items
       .map(({ preview, original, description }) => {
-        return `<ul class="gallery__item">
+        return `<div class="gallery__item">
     <a class="gallery__link" href="${original}">
       <img
         class="gallery__image"
@@ -17,8 +18,32 @@ function createGalleryItemsMarkup(items) {
         alt="${description}"
       />
     </a>
-  </ul>`;
+  </div>`;
       })
       .join('');
   }
+  const instance = basicLightbox.create(
+    `
+  <img width="1280" height="auto" src="">`,
+    {
+      onShow: (instance) => {
+        window.addEventListener('keydown', onEscKeyPress);
+      },
+      onClose: (instance) => {
+        window.removeEventListener('keydown', onEscKeyPress);
+      },
+    }
+  );
+  function onImgClick(e) {
+    e.preventDefault();
+    const datasetSource = e.target.dataset.source;
+    if (!datasetSource) return;
+    instance.element().querySelector('img').src = datasetSource;
+    instance.show();
+  }
+  function onEscKeyPress(e) {
+    if (e.code !== 'Escape') return;
+    instance.close();
+  }
+  
 console.log(galleryItems);
